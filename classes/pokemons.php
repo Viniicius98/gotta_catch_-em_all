893e638334267eb86b740d20a656d819b52cef56
Vinicius Silva <91093655+Viniicius98@users.noreply.github.com>
2024-04-17 17:57:01 -0300

    Adicionando novos método

--- classes/pokemons.php
+++ classes/pokemons.php
@@ -29,11 +29,29 @@ class MeuPokemon
         }
     }
 
+    private function ExibirTipo($data)
+    {
+
+        foreach ($data['types'] as $tipo) {
+            $nomeTipo = ucfirst($tipo['type']['name']);
+            $urlTipo = $tipo['type']['url'];
+
+            echo $nomeTipo;
+        }
+    }
+
     private function ExibirAltura($data)
     {
 
         $altura = (float) $data['height'] / 10;
         echo $altura;
+    }
+
+    private function ExibirPeso($data)
+    {
+
+        $peso = (float) $data['weight'] / 10;
+        echo $peso;
     }
     private function ExibirStatus($data)
     {
@@ -43,22 +61,23 @@ class MeuPokemon
             echo "$nomeStatus: $baseStat<br>";
         }
     }
-    private function ObterHabilidades($data)
+    private function ExibirHabilidades($data)
     {
 
         foreach ($data['abilities'] as $ability) {
-            echo $ability['ability']['name'] . ": " . $ability['is_hidden'] ? "(oculta)" : "" . "<br>";
+            echo ucfirst($ability['ability']['name'] . ($ability['is_hidden'] ? ' (Habilidade Oculta)' : '') . '<br/>');
         }
     }
 
-
-
-    private function ExibirChoros($data)
+    private function ExibirGritos($data)
     {
+        foreach ($data['forms'] as $form) {
 
-        echo "Latest: " . $data['latest'] . "<br>";
-        echo "Legacy: " . $data['legacy'] . "<br>";
-        echo "Formas: " . $data['forms'][0]['name'] . "<br>";
+            echo '<audio controls>';
+            echo '<source src="' . $data['cries']['latest'] . '" type="audio/ogg">';
+            echo 'Seu navegador não suporta áudio HTML5.';
+            echo '</audio><br>';
+        }
     }
 
     private function ExibirJogos($data)
@@ -72,13 +91,59 @@ class MeuPokemon
 
 
 
-    private function ExibirMovimentos($data)
-    {
+    // private function ExibirEvolucoes($data)
+    // {
+    //     // Verifica se a URL da cadeia de evolução está definida
+    //     if (isset($data['species']['url'])) {
+    //         // Obtém a URL da cadeia de evolução
+    //         $evolution_chain_url = $data['species']['url'];
 
-        foreach ($data['moves'] as $move) {
-            echo $move['move']['name'] . ": " . $move['version_group_details'][0]['move_learn_method']['name'] . "<br>";
-        }
-    }
+    //         // Faz uma solicitação HTTP para obter os detalhes da cadeia de evolução
+    //         $evolution_chain_data = file_get_contents($evolution_chain_url);
+    //         $evolution_chain_data = json_decode($evolution_chain_data, true);
+
+    //         // Verifica se os dados da cadeia de evolução foram obtidos com sucesso
+    //         if ($evolution_chain_data !== null) {
+    //             // Obtém a URL específica da cadeia de evolução
+    //             $evolution_chain_url = $evolution_chain_data['evolution_chain']['url'];
+
+    //             // Faz uma solicitação HTTP para obter os detalhes específicos da cadeia de evolução
+    //             $evolution_chain_details = file_get_contents($evolution_chain_url);
+    //             $evolution_chain_details = json_decode($evolution_chain_details, true);
+
+    //             // Verifica se os detalhes da cadeia de evolução foram obtidos com sucesso
+    //             if ($evolution_chain_details !== null) {
+    //                 // Itera sobre as etapas de evolução
+    //                 $current_stage = $evolution_chain_details['chain'];
+    //                 while ($current_stage) {
+    //                     // Exibe o nome da espécie atual
+    //                     echo "Nome da espécie: " . $current_stage['species']['name'] . "<br>";
+
+
+    //                     // Verifica se existem evoluções para esta etapa
+    //                     if (!empty($current_stage['evolves_to'])) {
+    //                         // Se houver, avança para a próxima etapa de evolução
+    //                         $current_stage = $current_stage['evolves_to'][0];
+    //                     } else {
+    //                         // Se não houver, encerra o loop
+    //                         break;
+    //                     }
+    //                 }
+    //             } else {
+    //                 // Se houve um problema ao obter os detalhes da cadeia de evolução
+    //                 echo "Erro ao obter os detalhes da cadeia de evolução.";
+    //             }
+    //         } else {
+    //             // Se houve um problema ao obter os dados da cadeia de evolução
+    //             echo "Erro ao obter os dados da cadeia de evolução.";
+    //         }
+    //     } else {
+    //         // Se a URL da cadeia de evolução não estiver definida
+    //         echo "URL da cadeia de evolução não encontrada.";
+    //     }
+    // }
+
+
 
 
 
--- pegarPokemon.php
+++ pegarPokemon.php
@@ -24,6 +24,7 @@ require_once "include/header.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Meu Pokemon</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
 <body>
@@ -31,14 +32,31 @@ require_once "include/header.php";
         <div>
             <h1>Gotta Catch ’Em All!</h1>
             <h3>Voce encontrou o pokemon!! </h3>
-            <h3><?= $pokemon->__get('pokemon') ?> </h3>
+            <h3><?= ucfirst($pokemon->__get('pokemon')) ?> </h3>
+            <section>
+                <p>Tipo: <?php $pokemon->PegarPokemon($pokemon->__get('pokemon'), ['ExibirTipo']); ?></p>
+            </section>
             <section>
                 <?php $pokemon->PegarPokemon($pokemon->__get('pokemon'), ['ExibirSprite']); ?>
             </section>
+
             <section>
                 <h3>Altura</h3>
                 <p><?php $pokemon->PegarPokemon($pokemon->__get('pokemon'), ['ExibirAltura']); ?> metros</p>
             </section>
+            <section>
+                <h3>Peso</h3>
+                <p><?php $pokemon->PegarPokemon($pokemon->__get('pokemon'), ['ExibirPeso']); ?> Kg</p>
+            </section>
+            <section>
+                <h3>Habilidades</h3>
+                <p><?php $pokemon->PegarPokemon($pokemon->__get('pokemon'), ['ExibirHabilidades']); ?></p>
+            </section>
+            <section>
+                <h3>Grito</h3>
+                <?php $pokemon->PegarPokemon($pokemon->__get('pokemon'), ['ExibirGritos']); ?>
+            </section>
+
             <section>
                 <h3>Status</h3>
                 <?php $pokemon->PegarPokemon($pokemon->__get('pokemon'), ['ExibirStatus']); ?>
